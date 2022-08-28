5c01a36d83aaa268cab91624c867e32c56640c2f
tobias-stein <tobiasstein40@gmail.com>
2022-08-28 15:14:41 +0200

    add development and production dotenv configuration

---
+++ scripts/publish.sh
@@ -0,0 +1,1 @@
+npm run build-prod && npm publish --access public
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
 const path = require('path');
+const dotenv = require('dotenv-webpack');
 
-module.exports = {
-    entry: 
-    {
-        'nodejji': './src/index.ts',
-        'nodejji.min': './src/index.ts'
-    },
-    output: 
-    {
-        path: path.resolve(__dirname, 'umd'),
-        filename: '[name].js',
-        library: 'nodejji',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    resolve: 
-    {
-        extensions: ['.ts', '.tsx', '.js'],
-        mainFields: ['module', 'browser', 'main'],
-        alias: {
-            '@': path.resolve(__dirname, 'src/')
-        }
-    },
-    devtool: 'source-map',
-    plugins: [],
-    module: 
-    {
-        rules: 
-        [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
+module.exports = function(env, argv) {
+    
+    return {
+        entry: 
+        {
+            'nodejji': './src/index.ts',
+            'nodejji.min': './src/index.ts'
+        },
+        output: 
+        {
+            path: path.resolve(__dirname, 'umd'),
+            filename: '[name].js',
+            library: 'nodejji',
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
+        resolve: 
+        {
+            extensions: ['.ts', '.tsx', '.js'],
+            mainFields: ['module', 'browser', 'main'],
+            alias: {
+                '@': path.resolve(__dirname, 'src/')
             }
-        ]  
+        },
+        devtool: 'source-map',
+        plugins: [
+            new dotenv({ path: path.resolve(__dirname, `.env.${argv.mode}`) })
+        ],
+        module: 
+        {
+            rules: 
+            [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]  
+        }
     }
 };
